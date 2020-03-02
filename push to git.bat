602c5b438a0ab0337cbf8f23892876785e89761e
sulaganatha5 <shalini.ulaganathan@dxc.com>
2020-03-02 09:53:46 +0000

    Removed all files

--- push to git.bat
+++
@@ -1,8 +0,0 @@
-rm src\main\java\com\dxc\application\config\SecurityConfiguration.java
-copy C:\Users\Administrator\Desktop\ramesh\SecurityConfiguration.java src\main\java\com\dxc\application\config\
-cls
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/dxc-demo/flight-services.git
-git push -u origin master
