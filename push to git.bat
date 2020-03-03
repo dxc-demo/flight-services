6f17bc72490aa89053c7a4704cb0ff6f57628baa
sulaganatha5 <shalini.ulaganathan@dxc.com>
2020-03-03 04:21:45 +0000

    first commit

---
+++ push to git.bat
@@ -0,0 +1,10 @@
+rm src\main\java\com\dxc\application\config\SecurityConfiguration.java
+copy C:\Users\Administrator\Desktop\ramesh\SecurityConfiguration.java src\main\java\com\dxc\application\config\
+cls
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/dxc-demo/flight-services.git
+git fetch
+git.exe pull --progress -v --no-rebase --allow-unrelated-histories "origin" master
+git push -u origin master
