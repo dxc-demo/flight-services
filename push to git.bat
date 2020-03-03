e3d6374c7093f1592cd8557657710c4c49beb979
sulaganatha5 <shalini.ulaganathan@dxc.com>
2020-03-03 05:34:00 +0000

    first commit

--- push to git.bat
+++ push to git.bat
@@ -6,5 +6,6 @@ git add .
 git commit -m "first commit"
 git remote add origin https://github.com/dxc-demo/flight-services.git
 git fetch
+cls
 git.exe pull --progress -v --no-rebase --allow-unrelated-histories "origin" master
 git push -u origin master
